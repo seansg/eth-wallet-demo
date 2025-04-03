07c6476e977708ad6a24106b136be1c950249d00
sean <seanwang@goldenio.com>
2025-04-03 23:21:13 +0800

    add Nav

---
+++ src/app/components/Nav.tsx
@@ -0,0 +1,27 @@
+import Burger from "@/app/images/icons/burger.svg"
+import Image from "next/image";
+
+const Nav = () => {
+  return (
+    <header className="border-grid sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+			<div className="container-wrapper">
+				<div className="container flex h-14 items-center gap-2 md:gap-4">
+
+					<a className="mx-4 flex items-center gap-2 lg:mx-6" href="/">
+						<span className="font-bold inline-block">Wallet Demo</span>
+					</a>
+
+					<div className="ml-auto flex items-center gap-2 md:flex-1 md:justify-end pr-2">
+						<nav className="flex items-center gap-0.5">
+							<button type="button" className="text-default lg:hidden rounded-lg hover:bg-hover" aria-label="Menu">
+								<Image src={Burger} alt={""} width={24} height={24}/>
+							</button>
+						</nav>
+					</div>
+				</div>
+			</div>
+		</header>
+  )
+}
+
+export default Nav
